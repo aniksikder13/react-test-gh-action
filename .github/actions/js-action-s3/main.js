34b09cec95eb3157fb3fecb44a18bf913a636339
Anik Sikder <aniksikder9501@gmail.com>
2025-12-13 19:15:05 +0600

    Change console to core in javascript actions

--- .github/actions/js-action-s3/main.js
+++ .github/actions/js-action-s3/main.js
@@ -3,5 +3,5 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 (function(){
-    console.log("This is S3 upload action ZZzzzXZZ");   
+    core.notice('Starting S3 upload action');
 })()
