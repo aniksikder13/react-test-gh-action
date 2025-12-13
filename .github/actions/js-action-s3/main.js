544a94eb6cc3a653c2599974c7a0a8a8aac6063d
Anik Sikder <aniksikder9501@gmail.com>
2025-12-13 21:59:57 +0600

    Implement s3 uploader actions in javascript

--- .github/actions/js-action-s3/main.js
+++ .github/actions/js-action-s3/main.js
@@ -3,5 +3,10 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 (function(){
-    core.notice('Starting S3 upload action');
+    const bucketName = core.getInput('bucket-name', { required: true });
+    const sourceDir = core.getInput('source-dir', { required: true });
+    const region = core.getInput("bucket-region", { required: true });
+    
+    // Upload to S3 using AWS CLI
+    exec.exec(`aws s3 sync ${sourceDir} s3://${bucketName} --region ${region}`);
 })()
