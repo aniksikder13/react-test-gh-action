ac1d22752cfb854d02b4adba15cc4e16f60cb02b
Anik Sikder <aniksikder9501@gmail.com>
2025-12-13 19:13:13 +0600

    Add javascript actions

---
+++ .github/actions/js-action-s3/main.js
@@ -0,0 +1,7 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+(function(){
+    console.log("This is S3 upload action ZZzzzXZZ");   
+})()
